e528489523b1ad6e38149a0683f944f6d281d615
abubakarseddek1999 <abubakarseddek1999@gmail.com>
2023-11-06 13:48:02 +0600

    verifytoken functionality add

--- index.js
+++ index.js
@@ -20,6 +20,30 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+// middleware
+const logger =(req,res,next)=>{
+    console.log('log: info',req.method,req.url);
+    next();
+}
+const verifyToken =(req,res,next)=>{
+    const token =req?.cookies?.token;
+    // console.log('token in the middleware', token);
+
+    if(!token){
+        return res.status(401).send({message: 'unauthorized access'})
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+        if(err){
+            return res.status(401).send({message: 'unauthorized access'})
+        }
+        req.user =decoded;
+        next();
+    })
+
+}
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
