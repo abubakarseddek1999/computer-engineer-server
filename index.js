bff8e2017d1c23e655a28c48bc28bbd397613c76
abubakarseddek1999 <abubakarseddek1999@gmail.com>
2023-11-06 13:22:28 +0600

    add create and read functionality of booking data

--- index.js
+++ index.js
@@ -29,7 +29,32 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
+      // Booking
+
+      app.get('/bookings',logger,verifyToken, async(req,res)=>{
+        console.log("hi man" ,req.query.email);
+        console.log('token owner info', req.user);
+        if(req.user.email !== req.query.email ){
+            return res.status(403).send({message: 'forbidden access'})
+        }
+        let query ={};
+        if(req.query?.email){
+            query ={email: req.query.email}
+        }
+
+        const result = await bookingCollection.find(query).toArray();
+        res.send(result);
+    })
+
+    app.post('/bookings', async(req, res)=>{
+        const booking= req.body;
+        console.log(booking);
+        const result =await bookingCollection.insertOne(booking);
+        res.send(result);
+
+
+    })
   }
 }
 run().catch(console.dir);
