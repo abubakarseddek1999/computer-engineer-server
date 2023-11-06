1914ec7f26ca87b6d29b57e54f42854ad9012008
abubakarseddek1999 <abubakarseddek1999@gmail.com>
2023-11-06 12:53:08 +0600

    basic server set

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+
+require('dotenv').config()
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('doctor is running')
+})
+
+
+app.listen(port, () => {
+    console.log(`Car Doctor is running on port ${port}`);
+})
